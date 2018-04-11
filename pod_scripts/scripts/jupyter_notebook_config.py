431ae003b2185bf3c2f6ff05378ad2bcbaaaa52e
Kenan Erdogan <kenan.erdogan@gesis.org>
2018-04-11 10:12:49 +0200

    pod scripts: get notebook password from env variable

--- pod_scripts/scripts/jupyter_notebook_config.py
+++ pod_scripts/scripts/jupyter_notebook_config.py
@@ -3,7 +3,6 @@
 # Set ip to '*' to bind on all interfaces (ips) for the public server
 c.NotebookApp.ip = '*'
 c.NotebookApp.base_url = '/pod-scripts/'
-c.NotebookApp.password = u''
 c.NotebookApp.open_browser = False
 
 # It is a good idea to set a known, fixed port for server access
