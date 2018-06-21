c81acd49c16ddd890cce608c19d7e8cb01c15564
Kenan Erdogan <kenan.erdogan@gesis.org>
2018-06-21 12:35:51 +0200

    user image: disable quit button

--- jupyterhub/docker/singleuser/extra_notebook_config.py
+++ jupyterhub/docker/singleuser/extra_notebook_config.py
@@ -1,5 +1,5 @@
 import os
-# c.NotebookApp.quit_button = False
+c.NotebookApp.quit_button = False
 # c.NotebookApp.extra_template_paths = ['/etc/jupyter/custom_notebook_templates']
 c.NotebookApp.jinja_template_vars = {'is_staging': os.environ.get('IS_STAGING') == "true",
                                      'is_production': os.environ.get('IS_PRODUCTION') == "true"}
