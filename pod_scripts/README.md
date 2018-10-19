7ddd5c9391d213ce09ac76be4dd51b45ec6853bb
Kenan Erdogan <kenan.erdogan@gesis.org>
2018-10-19 16:59:39 +0200

    rm pod_scripts

--- pod_scripts/README.md
+++
@@ -1,3 +0,0 @@
-These scripts are take from
-[mybinder.org-deploy](https://github.com/jupyterhub/mybinder.org-deploy/tree/master/scripts)
-and updated for ORC project.
--- pod_scripts/scripts/jupyter_notebook_config.py
+++
@@ -1,9 +0,0 @@
-# jupyter notebook --generate-config
-
-# Set ip to '*' to bind on all interfaces (ips) for the public server
-c.NotebookApp.ip = '*'
-c.NotebookApp.base_url = '/pod-scripts/'
-c.NotebookApp.open_browser = False
-
-# It is a good idea to set a known, fixed port for server access
-c.NotebookApp.port = 5000
