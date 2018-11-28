54bf908b38a0baf761446868742e5367d832d7e5
Kenan Erdogan <kenanerdogan@gmail.com>
2018-11-28 09:36:27 +0100

    fix csv file path

--- orc_site/utilities.py
+++ orc_site/utilities.py
@@ -1,8 +1,10 @@
 import csv
+import os
 
 
 def get_created_by_gesis():
-    with open('created_by_gesis.csv', 'r') as f:
+    csv_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'created_by_gesis.csv')
+    with open(csv_file, 'r') as f:
         reader = csv.reader(f)
         header = next(reader)
         created_by_gesis = list(reader)
