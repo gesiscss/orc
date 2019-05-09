877359b997478696d84f2de8e4be35f123caee15
Kenan Erdogan <kenan.erdogan@gesis.org>
2019-05-09 08:37:24 +0200

    orc-site: use default logging handler with info level

--- orc_site/app.py
+++ orc_site/app.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 if not app.debug:
     # configure flask.app logger
     import logging
-    sh = logging.StreamHandler()
-    formatter = logging.Formatter('[%(asctime)s] %(levelname)s in %(module)s: %(message)s')
-    sh.setFormatter(formatter)
-    app.logger.addHandler(sh)
+    # sh = logging.StreamHandler()
+    # formatter = logging.Formatter('[%(asctime)s] %(levelname)s in %(module)s: %(message)s')
+    # sh.setFormatter(formatter)
+    # app.logger.handlers = [sh]
     app.logger.setLevel(logging.INFO)
     # reverse proxy fix
     from werkzeug.middleware.proxy_fix import ProxyFix
