6b589c9ddea015d2ec9818be9fae102b443143bb
Kenan Erdogan <kenan.erdogan@gesis.org>
2019-05-16 10:18:25 +0200

    update backup script: use concurrency when compressing user data dirs delete backup of last month when it is 16th of current month use python base image instead of ubuntu

--- nfs_provisioner/backup/docker/backup.py
+++ nfs_provisioner/backup/docker/backup.py
@@ -1,7 +1,9 @@
-import os
+from os import environ, mkdir, listdir
+from os.path import join, exists, expanduser
 import json
 import shutil
 import logging
+from concurrent.futures import ProcessPoolExecutor, as_completed
 
 from kubernetes import client, config
 from datetime import datetime, timedelta
@@ -9,50 +11,67 @@ from time import time
 from subprocess import run, PIPE
 
 
+def mkdir_p(dir_path):
+    if not exists(dir_path):
+        mkdir(dir_path)
+
+
+def archive(pvs_backup_path, pv_dir_name, pvc_name):
+    # format -> xztar: xz’ed tar-file (if the lzma module is available)
+    # compress and save under the name of pvc
+    filename = shutil.make_archive(join(pvs_backup_path, pvc_name),
+                                   'xztar',
+                                   join(environ['PV_FOLDER'], pv_dir_name))
+    return filename
+
+
 def backup():
     start_time = time()
 
-    # create backup directory
+    # create backup directories
     year, month, day = str(datetime.now().date()).split('-')
-    year_path = os.path.join(os.environ['BACKUP_FOLDER'], year)
-    if not os.path.exists(year_path):
-        os.mkdir(year_path)
-    month_path = os.path.join(year_path, month)
-    if not os.path.exists(month_path):
-        os.mkdir(month_path)
-    day_path = os.path.join(month_path, day)
-    os.mkdir(day_path)
+    year_path = join(environ['BACKUP_FOLDER'], year)
+    mkdir_p(year_path)
+    month_path = join(year_path, month)
+    mkdir_p(month_path)
+    day_path = join(month_path, day)
+    mkdir(day_path)
 
     # setup logging
     logger = logging.getLogger()  # root logger
-    file_handler = logging.FileHandler(os.path.join(day_path, 'backup.log'))
+    file_handler = logging.FileHandler(join(day_path, 'backup.log'))
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     file_handler.setFormatter(formatter)
     # file_handler.setLevel(logging.INFO)
     logger.addHandler(file_handler)
     logger.setLevel(logging.INFO)
 
-    # dump all databases
-    databases = ['jhub_db', 'bhub_db', 'jhub_test_db', 'bhub_test_db']
-    logger.info('dumping databases: {}'.format(', '.join(databases)))
-    os.mkdir(os.path.expanduser('~/.ssh'))
+    # dump all production databases
+    databases = ['jhub_db', 'bhub_db', 'gallery', 'keycloak']
+    logger.info(f"## dumping databases: {', '.join(databases)}")
+    mkdir(expanduser('~/.ssh'))
     command = 'ssh-keyscan -H 194.95.75.9 > ~/.ssh/known_hosts'
     cp = run(command, shell=True, stdout=PIPE, stderr=PIPE)
-    logger.info('{} - {} - {} - {}'.format(cp.args, cp.returncode, cp.stdout, cp.stderr))
+    logger.info(f"#### command: {cp.args} -> {cp.returncode} - stdout: {cp.stdout}")
+    if cp.stderr:
+        logger.error(f"##### error for command {cp.args}: {cp.stderr}")
     for db in databases:
-        dump_path = os.path.join(day_path, '{}.sql.gz'.format(db))
+        dump_path = join(day_path, f'{db}.sql.gz')
         # take password from SSHPASS env variable
-        command = 'sshpass -e ssh iuser@194.95.75.9 "pg_dump -Fp -h "localhost" -U "jhub" -d "{}"" | gzip > {}'.\
-                  format(db, dump_path)
+        command = f'sshpass -e ssh iuser@194.95.75.9 ' \
+                  f'"pg_dump -Fp -h "localhost" -U "jhub" -d "{db}"" ' \
+                  f'| gzip > {dump_path}'
         cp = run(command, shell=True, stdout=PIPE, stderr=PIPE)
-        logger.info('{}:-> {} - {} - {} - {}'.format(db, cp.args, cp.returncode, cp.stdout, cp.stderr))
-    logger.info('Done: dumping databases')
+        logger.info(f"#### command for {db}: {cp.args} -> {cp.returncode} - stdout: {cp.stdout}")
+        if cp.stderr:
+            logger.error(f"##### error for command {cp.args}: {cp.stderr}")
+    done_db_time = time()
+    logger.info(f'## Done: dumping databases: {timedelta(seconds=done_db_time-start_time)}\n\n')
 
-    # Save config files for all pvcs in jhub-ns
-    logger.info('Save config files for all pvcs in jhub-ns')
-    pvcs_backup_path = os.path.join(day_path, 'pvcs')
-    if not os.path.exists(pvcs_backup_path):
-        os.mkdir(pvcs_backup_path)
+    # Save config files of all production PVCs
+    logger.info('## Save config files of all production PVCs')
+    pvcs_backup_path = join(day_path, 'pvcs')
+    mkdir_p(pvcs_backup_path)
     pvc_template = """apiVersion: v1
 kind: PersistentVolumeClaim
 metadata:
@@ -60,7 +79,10 @@ metadata:
     hub.jupyter.org/username: {username}
   labels:
     app: {app}
+    chart: {chart}
+    component: {component}
     heritage: {heritage}
+    release: {release}
   name: {name}
   namespace: {namespace}
 spec:
@@ -80,52 +102,85 @@ spec:
             pv_dict[pvc.spec.volume_name] = pvc.metadata.name
             d = {'username': pvc.metadata.annotations['hub.jupyter.org/username'],
                  'app': pvc.metadata.labels['app'],
+                 # TODO get() -> [] after first backup
+                 'chart': pvc.metadata.labels.get('chart', 'jupyterhub-0.8.2'),
+                 'component': pvc.metadata.labels.get('component', 'singleuser-storage'),
                  'heritage': pvc.metadata.labels['heritage'],
+                 'release': pvc.metadata.labels.get('release', 'jhub'),
                  'name': pvc.metadata.name,
                  'namespace': pvc.metadata.namespace,
                  'access_mode': pvc.spec.access_modes[0],
                  'storage': pvc.spec.resources.requests['storage']}
-            with open('{}/{}.yaml'.format(pvcs_backup_path, pvc.metadata.name), 'w') as f:
+            with open(join(pvcs_backup_path, f'{pvc.metadata.name}.yaml'), 'w') as f:
                 f.write(pvc_template.format(**d))
         # grafana and prometheus PVs
         elif pvc.metadata.namespace == 'default' and \
             ('grafana' in pvc.metadata.name or 'prometheus' in pvc.metadata.name):
             pv_dict[pvc.spec.volume_name] = pvc.metadata.name
-    with open('{}/pv_dict.json'.format(day_path), 'w') as fp:
+        # efk-stack
+        elif pvc.metadata.namespace == 'efk-stack-ns':
+            pv_dict[pvc.spec.volume_name] = pvc.metadata.name
+    with open(f'{day_path}/pv_dict.json', 'w') as fp:
         json.dump(pv_dict, fp, indent=4)
-    logger.info('Done: Save config files for all pvcs in jhub-ns')
+    done_config_files = time()
+    logger.info(f'## Done: Save config files of all production PVCs: '
+                f'{timedelta(seconds=done_config_files-done_db_time)}\n\n')
 
-    # back up each user folder separately and back up grafana and prometheus PVs
-    logger.info('back up each user folder separately and back up grafana and prometheus PVs')
-    pvs_backup_path = os.path.join(day_path, 'pvs')
-    if not os.path.exists(pvs_backup_path):
-        os.mkdir(pvs_backup_path)
-    count = 0
-    for pv_dir_name in os.listdir(os.environ['PV_FOLDER']):
-        # filter out pvs of jhub-test-ns
+    # backup PVs
+    logger.info('## Back up nfs shares separately: user folders and also grafana, prometheus and efk-stack data')
+    pvs_backup_path = join(day_path, 'pvs')
+    mkdir_p(pvs_backup_path)
+    pvs = []
+    for pv_dir_name in listdir(environ['PV_FOLDER']):
+        # filter out nfs files and PVs of staging
         if pv_dir_name.startswith('pvc-') and pv_dir_name in pv_dict:
-            # format -> xztar: xz’ed tar-file (if the lzma module is available)
-            # compress and save under the name of pvc
-            filename = shutil.make_archive(os.path.join(pvs_backup_path, pv_dict[pv_dir_name]),
-                                           'xztar',
-                                           os.path.join(os.environ['PV_FOLDER'], pv_dir_name))
-            count += 1
-            logger.info(filename)
-    logger.info('Done: {} PVs'.format(count))
+            pvs.append(pv_dir_name)
+    max_workers = int(environ.get("MAX_WORKERS", 5))
+    with ProcessPoolExecutor(max_workers=max_workers) as executor:
+        jobs = {}
+        pvs_left = len(pvs)
+        pvs_iter = iter(pvs)
 
-    # delete backup data older than 1 month 1 day
-    logger.info('delete backup data older than 1 month 1 day')
-    if day == '01':
-        if month == '01' or month == '02':
-            previous_year_path = os.path.join(os.environ['BACKUP_FOLDER'], str(int(year) - 1))
-            previous_month_path = os.path.join(previous_year_path, '{}'.format(str(10+int(month))))
+        success = 0
+        fail = 0
+        while pvs_left:
+            for pv_dir_name in pvs_iter:
+                job = executor.submit(archive, pvs_backup_path, pv_dir_name, pv_dict[pv_dir_name])
+                jobs[job] = pv_dir_name
+                if len(jobs) == max_workers:  # limit # jobs with max_workers
+                    break
+
+            for job in as_completed(jobs):
+                pvs_left -= 1
+                pv_dir_name = jobs[job]
+                try:
+                    filename = job.result()
+                except Exception:
+                    fail += 1
+                    logger.exception(pv_dir_name)
+                else:
+                    success += 1
+                    logger.info(filename)
+
+                del jobs[job]
+                break  # to add a new job, if there is any
+    logger.info(f'## Done nfs shares ({success} PVs, failed {fail}): {timedelta(seconds=time()-done_config_files)}\n\n')
+
+    # Delete backup folder of last month
+    if day == '16':
+        logger.info('## delete backup data of last month')
+        if month == '01':
+            previous_year_path = join(environ['BACKUP_FOLDER'], str(int(year) - 1))
+            previous_month_path = join(previous_year_path, '12')
+            # previous_month_path = join(previous_year_path, '{}'.format(str(10or11 + int(month))))
         else:
-            previous_month_path = os.path.join(year_path, '{:02}'.format(int(month) - 2))
-        if os.path.exists(previous_month_path):
-            logger.info('deleting {}'.format(previous_month_path))
+            previous_month_path = join(year_path, '{:02}'.format(int(month) - 1))
+        if exists(previous_month_path):
+            logger.info(f'### deleting {previous_month_path}')
             shutil.rmtree(previous_month_path, ignore_errors=True)
-    logger.info('Done: delete backup data older than 1 month 1 day')
-    logger.info('Backup duration: {}'.format(timedelta(seconds=time()-start_time)))
+        logger.info('## Done: delete backup data of last month')
+
+    logger.info(f'Backup duration: {timedelta(seconds=time()-start_time)}')
 
 
 if __name__ == '__main__':
