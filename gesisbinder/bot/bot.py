71789d0b070f672afcc88657149b0d1e1f93b68a
Kenan Erdogan <kenanerdogan@gmail.com>
2019-09-08 07:48:52 +0200

    fix getting last commit hash of r2d

--- gesisbinder/bot/bot.py
+++ gesisbinder/bot/bot.py
@@ -141,7 +141,7 @@ class Bot:
 
         if repo == 'repo2docker':
             commit_message = 'repo2docker: https://github.com/jupyter/repo2docker/compare/{}...{}'.format(
-                self.commit_info['repo2docker']['live'], self.commit_info['repo2docker']['latest'].rstrip('.dirty').split('.')[-1])
+                self.commit_info['repo2docker']['live'], self.commit_info['repo2docker']['latest'].split('.dirty')[0].split('.')[-1])
         elif repo == 'binderhub':
             commit_message = 'binderhub: https://github.com/jupyterhub/binderhub/compare/{}...{}'.format(
                 self.commit_info['binderhub']['live'], self.commit_info['binderhub']['latest'])
@@ -204,7 +204,7 @@ class Bot:
         if repo == 'repo2docker':
             compare_url = 'https://github.com/jupyter/repo2docker/compare/{}...{}'.format(
                                 self.commit_info['repo2docker']['live'],
-                                self.commit_info['repo2docker']['latest'].rstrip('.dirty').split('.')[-1])
+                                self.commit_info['repo2docker']['latest'].split('.dirty')[0].split('.')[-1])
         elif repo == 'binderhub':
             compare_url = 'https://github.com/jupyterhub/binderhub/compare/{}...{}'.format(
                                 self.commit_info['binderhub']['live'],
