d4c816e313c0a04735b7c1ac8ed401d5dba6fdf5
Kenan Erdogan <kenanerdogan@gmail.com>
2019-10-14 11:43:17 +0200

    fix image cleaner

--- gesisbinder/image_cleaner/image_cleaner.py
+++ gesisbinder/image_cleaner/image_cleaner.py
@@ -1,3 +1,7 @@
+"""
+Some parts are taken from binderhub's image-cleaner
+(https://github.com/jupyterhub/binderhub/tree/master/helm-chart/images/image-cleaner)
+"""
 from datetime import datetime, timedelta
 from urllib.parse import unquote
 from time import time, sleep
@@ -28,8 +32,10 @@ def clean_images():
 
     image_prefix = "gesiscss/orc-binder-"  # TODO get as config
     for days in range(7, -1, -1):  # TODO get 7 as config
+        # TODO get 50 as config
+        available_low_limit = 50  # in percentage
         available = get_available_disk_space()
-        if available > 50:  # TODO get 50 as config
+        if available > available_low_limit:
             # cleaning is done when there is enough free disk space
             logging.info(f"There is enough free disk space ({available}%). Stop deleting.")
             break
@@ -62,6 +68,9 @@ def clean_images():
 
         _launches = []
         for l in launches:
+            # get launches on GESIS only
+            if l["origin"] not in ["notebooks.gesis.org", "gesis.mybinder.org"]:
+                continue
             if l["provider"] in ["Git", "GitLab"]:
                 spec = l["spec"].rsplit("/", 1)[0]
                 spec = unquote(spec)
@@ -73,6 +82,8 @@ def clean_images():
                 spec = spec.split(".git")[0]
             _launches.append((spec, l["provider"].lower()))
 
+        delay = 5  # TODO get 5 as config
+        deleted = 0
         launches = _launches
         for image in client.images.list():
             if not image.tags:
@@ -81,19 +92,48 @@ def clean_images():
             tag = image.tags[0]
             if not tag.startswith(image_prefix):
                 continue
+
             # image.labels["repo2docker.repo"] gives us repo url
             # image.labels["repo2docker.version"] gives us repo2docker version
             # image.labels["repo2docker.ref"] gives us resolved ref (image tag)
+            # delete dockerfile repos always, because they dont have r2d labels (unless maintainer adds them)
             if "repo2docker.repo" not in image.labels or \
-               bool([True for spec, provider in launches
-                     if spec in image.labels["repo2docker.repo"] and provider in image.labels["repo2docker.repo"]]):
-                # delete dockerfile repos always, because they dont have r2d labels (unless maintainer adds them)
+                not bool([True for spec, provider in launches
+                          if spec in image.labels["repo2docker.repo"] and provider in image.labels["repo2docker.repo"]]):
                 logging.info(f"Deleting {tag} with labels: {image.labels}")
-                client.images.remove(tag)
-                sleep(5)
+                try:
+                    client.images.remove(tag)
+                    # client.images.remove(image=image.id, force=True)
+                    # logging.info(f'Removed {name}')
+                    # Delay between deletions.
+                    # A sleep here avoids monopolizing the Docker API with deletions.
+                    sleep(delay)
+                except docker.errors.APIError as e:
+                    if e.status_code == 409:
+                        # This means the image can not be removed right now
+                        logging.info(f'Failed to remove {tag}, skipping this image')
+                        logging.info(str(e))
+                    elif e.status_code == 404:
+                        logging.info(f'{tag} not found, probably already deleted')
+                    else:
+                        raise
+                except requests.exceptions.ReadTimeout:
+                    logging.warning(f'Timeout removing {tag}')
+                    # Delay longer after a timeout, which indicates that Docker is overworked
+                    sleep(max(delay, 30))
+                else:
+                    deleted += 1
+                    if available > 50:
+                        # cleaning is done when there is enough free disk space
+                        logging.info(f"While deleting everything not used in last {days} days, "
+                                     f"reached to enough free disk space ({available}%). Stop deleting.")
+                        break
+            else:
+                logging.info(f"Skipping {tag} with labels: {image.labels}")
+
 
         available = get_available_disk_space()
-        logging.info(f"Free disk space after deleting {days} days: {available}%")
+        logging.info(f"Deleted {deleted} images. Free disk space after deleting {days} days: {available}%")
     logging.info(f"Duration: {timedelta(seconds=time()-start_time)}\n\n")
 
 
