65f6380881a1cfca9226e247e5901c588b76569e
Kenan Erdogan <kenanerdogan@gmail.com>
2019-10-15 07:51:01 +0200

    binder buttons, new way

--- gesisbinder/gesisbinder/files/etc/jupyter/jupyter_notebook_config.py
+++ gesisbinder/gesisbinder/files/etc/jupyter/jupyter_notebook_config.py
@@ -1,5 +1,11 @@
 import os
 c.NotebookApp.extra_template_paths.append('/etc/jupyter/templates')
+
+federation_host = 'https://mybinder.org'
+current_host = 'https://notebooks.gesis.org/binder'
 c.NotebookApp.jinja_template_vars.update({
-    'binder_url': os.environ.get('BINDER_URL', 'https://notebooks.gesis.org/binder'),
+    'binder_url': os.environ.get('BINDER_URL', federation_host).replace(current_host, federation_host),
+    'persistent_binder_url': os.environ.get('PERSISTENT_BINDER_URL', '').replace(current_host, federation_host),
+    'repo_url': os.environ.get('REPO_URL', ''),
+    'ref_url': os.environ.get('REF_URL', ''),
 })
