aa390db3a3a1c5bd58a427e2a31b1d3cd1eb63bd
Kenan Erdogan <kenanerdogan@gmail.com>
2019-10-15 09:23:19 +0200

    move appendix to values.yaml. fix button links for staging

--- gesisbinder/gesisbinder/files/etc/jupyter/jupyter_notebook_config.py
+++ gesisbinder/gesisbinder/files/etc/jupyter/jupyter_notebook_config.py
@@ -2,10 +2,16 @@ import os
 c.NotebookApp.extra_template_paths.append('/etc/jupyter/templates')
 
 federation_host = 'https://mybinder.org'
-current_host = 'https://notebooks.gesis.org/binder'
+current_hosts = ['https://notebooks.gesis.org/binder', 'https://notebooks-test.gesis.org/binder']
+binder_url = os.environ.get('BINDER_URL', federation_host)
+persistent_binder_url = os.environ.get('PERSISTENT_BINDER_URL', '')
+for h in current_hosts:
+    binder_url = binder_url.replace(h, federation_host)
+    persistent_binder_url = persistent_binder_url.replace(h, federation_host)
+
 c.NotebookApp.jinja_template_vars.update({
-    'binder_url': os.environ.get('BINDER_URL', federation_host).replace(current_host, federation_host),
-    'persistent_binder_url': os.environ.get('PERSISTENT_BINDER_URL', '').replace(current_host, federation_host),
+    'binder_url': binder_url,
+    'persistent_binder_url': persistent_binder_url,
     'repo_url': os.environ.get('REPO_URL', ''),
     'ref_url': os.environ.get('REF_URL', ''),
 })
