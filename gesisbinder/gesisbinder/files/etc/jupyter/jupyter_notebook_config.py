e2f81b2fea174da4a288e9a1ba360a7972d5b2c6
Kenan Erdogan <kenan.erdogan@gesis.org>
2019-12-02 15:05:28 +0100

    hardcode binder_launch_host to mybinder.org

--- gesisbinder/gesisbinder/files/etc/jupyter/jupyter_notebook_config.py
+++ gesisbinder/gesisbinder/files/etc/jupyter/jupyter_notebook_config.py
@@ -13,21 +13,14 @@ if V(notebook.__version__) < V('5.1.0'):
     c.NotebookApp.jinja_environment_options = {'extensions': ['jinja2.ext.i18n']}
 
 
-def make_federation_url(url):
-    federation_host = 'https://mybinder.org'
-    if not url:
-        return ''
-    url_parts = url.split('/v2/', 1)
-    return federation_host + '/v2/' + url_parts[-1]
-
-
-binder_launch_host = os.environ.get('BINDER_LAUNCH_HOST', '')
+# binder_launch_host = os.environ.get('BINDER_LAUNCH_HOST', '')
+binder_launch_host = 'https://mybinder.org/'
 binder_request = os.environ.get('BINDER_REQUEST', '')
 binder_persistent_request = os.environ.get('BINDER_PERSISTENT_REQUEST', '')
 
 c.NotebookApp.jinja_template_vars.update({
-    'binder_url': make_federation_url(binder_launch_host+binder_request),
-    'persistent_binder_url': make_federation_url(binder_launch_host+binder_persistent_request),
+    'binder_url': binder_launch_host+binder_request,
+    'persistent_binder_url': binder_launch_host+binder_persistent_request,
     'repo_url': os.environ.get('BINDER_REPO_URL', ''),
     'ref_url': os.environ.get('BINDER_REF_URL', ''),
 })
