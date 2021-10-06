bc41033cb104618a98d1f092adf6f657c832db99
Mridul Seth <seth.mridul@gmail.com>
2021-10-06 17:34:03 +0200

    clean up takeout api

--- takeout/image/app/main.py
+++ takeout/image/app/main.py
@@ -3,15 +3,21 @@ from fastapi.responses import FileResponse
 import requests
 import json
 import os
+
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
-with open(os.path.join(current_dir, '_secret_token.json')) as f:
+with open(os.path.join(current_dir, "_secret_token.json")) as f:
     data = json.load(f)
 app = FastAPI()
+
 
 @app.get("/usertakeout/{user_id}")
 async def user_data_takeout(user_id: str):
     # add a post notification to telegram bot
-    payload = {"chat_id" : data['chat_id'],  "text": "This is a test from takeout API.", "disable_notification": true}
-    requests.post(data['url'], data=json.dumps(payload))
-    return FileResponse(f'data6/databackup/{user_id}')
+    payload = {
+        "chat_id": data["chat_id"],
+        "text": "This is a test from takeout API.",
+        "disable_notification": "true",
+    }
+    requests.post(data["url"], data=json.dumps(payload))
+    return FileResponse(f"data6/databackup/{user_id}")
