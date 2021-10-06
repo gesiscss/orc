bd51a72edc1a1197d81e32f5f4815212f2f7a431
Mridul Seth <seth.mridul@gmail.com>
2021-10-06 17:21:00 +0200

    add logging

--- takeout/image/app/main.py
+++ takeout/image/app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+import requests
+import json
+
+with open('_secret_token.json', 'r') as f:
+    data = json.load(f)
 
 app = FastAPI()
 
 @app.get("/usertakeout/{user_id}")
 async def user_data_takeout(user_id: str):
+    # add a post notification to telegram bot
+    payload = {"chat_id" : data['chat_id'],  "text": "This is a test from takeout API.", "disable_notification": true}
+    requests.post(data['url'], data=json.dumps(payload))
     return FileResponse(f'data6/databackup/{user_id}')
