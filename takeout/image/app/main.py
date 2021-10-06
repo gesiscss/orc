338928a50ca393354d5db9e3b7d66a50b9166104
Mridul Seth <seth.mridul@gmail.com>
2021-10-06 16:53:17 +0200

    update backup path

--- takeout/image/app/main.py
+++ takeout/image/app/main.py
@@ -5,4 +5,4 @@ app = FastAPI()
 
 @app.get("/usertakeout/{user_id}")
 async def user_data_takeout(user_id: str):
-    return FileResponse(f'databackup/{user_id}')
+    return FileResponse(f'data6/databackup/{user_id}')
